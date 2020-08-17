9d99b748da5cf1f47e1fa0ec61feccb0ef7523ea
NaveedG <nganjani@protonmail.com>
2020-08-16 19:47:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-// Code your solution here!
+function printString(string) {
+  console.log(string[0])
+  if (string.length > 1) {
+    printString(string.slice(1))
+  } else return true
+}
 
+function reverseString(string) {
+  if (string === '') {
+    return string
+  } else {
+    return reverseString(string.slice(1)) + string[0]
+  }
+}
+
+function isPalindrome(string) {
+  if (string.length === 1) {
+    return true
+  } else if (string[0] === string[string.length - 1]) {
+    return isPalindrome(string.substring(1, string.length - 1))
+  } else return false
+}
+
+function addUpTo(arr, index) {
+  if (arr[0] === arr[index]) {
+    return arr[index]
+  } else {
+    return arr[index] + addUpTo(arr, --index)
+  }
+}
+
+function maxOf(arr) {
+  if (arr.length === 1) {
+    return arr[0]
+  } else {
+    return Math.max(arr.pop(), maxOf(arr))
+  }
+}
+
+function includesNumber(arr, element) {
+  if (arr.length > 0) {
+    return (arr[0] === element) ? true : includesNumber(arr.slice(1), element)
+  } else return false
+}
